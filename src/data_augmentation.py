30fed6572a82beafa1a4575036a362cbde504088
leiqne <joaquincastillaeguia7@gmail.com>
2024-10-21 13:54:29 -0500

    release/data-augmentation

--- src/data_augmentation.py
+++ src/data_augmentation.py
@@ -1,3 +1,22 @@
 import os
 import cv2
 import numpy as np
+
+def load_images(data_dir, target_size=(224,224)):
+    images = []
+    labels = []
+    
+    for label in os.listdir(data_dir):
+        label_dir = os.path.join(data_dir, label)
+        
+        if os.path.isdir(label_dir):
+            for img_file in os.listdir(label_dir):
+                img_path = os.path.join(label_dir, img_file)
+                # Solo carga archivos de imagen
+                if img_file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+                    img=cv2.imread(img_path)
+                    img=cv2.resize(img, target_size)
+                    images.append(img)
+                    labels.append(label)
+
+    return images, labels
--- src/data_processing.py
+++ src/data_processing.py
@@ -2,25 +2,8 @@ import os
 import numpy as np
 import cv2
 from sklearn.model_selection import train_test_split
+from data_augmentation import load_images
 
-def load_images(data_dir, target_size=(224,224)):
-    images = []
-    labels = []
-    
-    for label in os.listdir(data_dir):
-        label_dir = os.path.join(data_dir, label)
-        
-        if os.path.isdir(label_dir):
-            for img_file in os.listdir(label_dir):
-                img_path = os.path.join(label_dir, img_file)
-                # Solo carga archivos de imagen
-                if img_file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
-                    img=cv2.imread(img_path)
-                    img=cv2.resize(img, target_size)
-                    images.append(img)
-                    labels.append(label)
-
-    return images, labels
 
 def label_encoder(labels):
     unique_labels = np.unique(labels)
